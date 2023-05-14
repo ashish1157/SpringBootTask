86e04d96c5533a29d3aac44a7d8241a3d8aa0f05
ashish1157 <132757907+ashish1157@users.noreply.github.com>
2023-05-14 22:29:34 +0530

    Add files via upload

---
+++ Taskk/src/main/java/com/example/pro/TaskkApplication.java
@@ -0,0 +1,13 @@
+package com.example.pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskkApplication.class, args);
+	}
+
+}
---
+++ Taskk/src/test/java/com/example/pro/TaskkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
